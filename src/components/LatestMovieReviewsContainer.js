0d4a166442f34f59fee1bffbb233bee916173e67
Corban <sonnemanc@gmail.com>
2022-03-24 14:10:44 -0700

    commit tree 950f875cea3abc7fcfcc185cce31c3df1faf8490

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            reviews: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL)
+          .then(res => res.json())
+          .then(response => this.setState({ reviews: response.results }))
+    }
+
+    render() {
+        return (
+        <div className="latest-movie-reviews">
+            <h1>The Latest Reviews:</h1>
+            <MovieReviews reviews={this.state.reviews}/>
+        </div>)
+    }
+}
+
+export default LatestMovieReviewsContainer;
